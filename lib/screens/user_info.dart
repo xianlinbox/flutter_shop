8be0410e5552a487228b6601984102f781399f0e
xianning liu <xianlinbox@gmail.com>
2022-02-18 19:53:00 -0600

    add base screens

---
+++ lib/screens/user_info.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UserInfo extends StatelessWidget {
+  const UserInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('UserInfo'),
+      ),
+    );
+  }
+}
