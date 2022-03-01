5619f1ad38a237ea15a1cef62ea665a21153e0db
xianning liu <xianlinbox@gmail.com>
2022-03-01 15:02:22 -0600

    add quanity and more row for feed item

--- lib/feeds/feed_item.dart
+++ lib/feeds/feed_item.dart
@@ -12,7 +12,7 @@ class _FeedItemState extends State<FeedItem> {
   Widget build(BuildContext context) {
     return Container(
       width: 250,
-      height: 290,
+      height: 390,
       decoration: BoxDecoration(
           color: Theme.of(context).backgroundColor,
           borderRadius: BorderRadius.circular(6)),
@@ -26,9 +26,39 @@ class _FeedItemState extends State<FeedItem> {
                     maxHeight: MediaQuery.of(context).size.height * 0.3),
                 child: Image.network(
                   "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1629842712000",
-                  fit: BoxFit.fill,
+                  fit: BoxFit.fitWidth,
                 ))),
-        Text('FeedItemsss'),
+        const SizedBox(height: 10),
+        Container(
+          padding: const EdgeInsets.only(left: 3),
+          margin: const EdgeInsets.only(left: 5, right: 5, bottom: 2),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            const Text('iPhone XR',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
+            const Text('\$999.99',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Quantity: 12 left',
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w800)),
+                Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () {},
+                      borderRadius: BorderRadius.circular(16),
+                      child: const Icon(Icons.more_horiz, color: Colors.grey),
+                    ))
+              ],
+            )
+          ]),
+        ),
       ]),
     );
   }
