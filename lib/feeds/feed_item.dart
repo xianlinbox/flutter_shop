760832d5582751d30183cb1d0c95b56dad54b656
xianning liu <xianlinbox@gmail.com>
2022-03-02 08:38:53 -0600

    add badge placeholder

--- lib/feeds/feed_item.dart
+++ lib/feeds/feed_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
 
 class FeedItem extends StatefulWidget {
   const FeedItem({Key? key}) : super(key: key);
@@ -19,17 +20,30 @@ class _FeedItemState extends State<FeedItem> {
             color: Theme.of(context).backgroundColor,
             borderRadius: BorderRadius.circular(6)),
         child: Column(children: [
-          ClipRRect(
-              borderRadius: BorderRadius.circular(2),
-              child: Container(
-                  width: double.infinity,
-                  constraints: BoxConstraints(
-                      minHeight: 100,
-                      maxHeight: MediaQuery.of(context).size.height * 0.3),
-                  child: Image.network(
-                    "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1629842712000",
-                    fit: BoxFit.fitWidth,
-                  ))),
+          Stack(children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(2),
+                child: Container(
+                    width: double.infinity,
+                    constraints: BoxConstraints(
+                        minHeight: 100,
+                        maxHeight: MediaQuery.of(context).size.height * 0.3),
+                    child: Image.network(
+                      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1629842712000",
+                      fit: BoxFit.fitWidth,
+                    ))),
+            Positioned(
+              right: 0,
+              child: Badge(
+                toAnimate: false,
+                shape: BadgeShape.square,
+                badgeColor: Colors.purple,
+                borderRadius: BorderRadius.circular(4),
+                badgeContent:
+                    const Text('New', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ]),
           const SizedBox(height: 10),
           Container(
             padding: const EdgeInsets.only(left: 3),
