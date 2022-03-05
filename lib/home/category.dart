af0f5a8c37444a29ce02a5728fca6b8ac74d7575
xianning liu <xianlinbox@gmail.com>
2022-03-04 18:27:39 -0600

    style Category widget

--- lib/home/category.dart
+++ lib/home/category.dart
@@ -17,22 +17,35 @@ class Category extends StatelessWidget {
             "https://img.ti-media.net/wp/uploads/sites/46/2021/06/GettyImages-1249466095-1-920x518.jpg"
       },
       {
-        "name": "Beauty",
+        "name": "Electronics",
         "image":
-            "https://img.ti-media.net/wp/uploads/sites/46/2021/06/GettyImages-1249466095-1-920x518.jpg"
+            "https://img2.exportersindia.com/product_images/bc-full/2019/2/6114156/electrical-electronic-1550471706-4727918.jpeg"
+      },
+      {
+        "name": "Sports",
+        "image":
+            "https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/sports-1584678012.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*"
+      },
+      {
+        "name": "Grocery",
+        "image":
+            "http://media.foxbusiness.com/BrightCove/854081161001/201912/951/854081161001_6118487245001_6118485083001-vs.jpg"
       }
     ];
 
     return SizedBox(
-      width: 100,
+      width: MediaQuery.of(context).size.width,
       height: 150,
       child: ListView.builder(
           itemCount: _categories.length,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
-            return _CategoryItem(
-                name: _categories[index]["name"]!,
-                imageUrl: _categories[index]["image"]!);
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _CategoryItem(
+                  name: _categories[index]["name"]!,
+                  imageUrl: _categories[index]["image"]!),
+            );
           }),
     );
   }
@@ -56,20 +69,31 @@ class _CategoryItem extends StatelessWidget {
       width: 100,
       height: 150,
       decoration: BoxDecoration(
+        color: Theme.of(context).backgroundColor,
         borderRadius: BorderRadius.circular(10),
       ),
       child: Column(children: [
-        SizedBox(
+        Container(
           width: 100,
           height: 100,
+          decoration: BoxDecoration(
+            // border: Border.all(color: Colors.grey, width: 1),
+            borderRadius: BorderRadius.circular(10),
+          ),
           child: Image.network(
             _imageUrl,
             fit: BoxFit.fill,
           ),
         ),
+        const SizedBox(
+          height: 5,
+        ),
         Text(
           _name,
-          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+          style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w800,
+              color: Theme.of(context).textSelectionTheme.selectionColor),
         )
       ]),
     );
