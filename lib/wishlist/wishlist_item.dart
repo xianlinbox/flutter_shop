ad41e6d51f66dd62a01e4ceb264dc0c1cdbf8c14
xianning liu <xianlinbox@gmail.com>
2022-03-07 08:44:53 -0600

    add delete button for wishlist

--- lib/wishlist/wishlist_item.dart
+++ lib/wishlist/wishlist_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/shared/colors.dart';
 
 class WishlistItem extends StatelessWidget {
   const WishlistItem({Key? key}) : super(key: key);
@@ -7,44 +8,65 @@ class WishlistItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {},
-      child: Container(
-        height: 100,
-        margin: const EdgeInsets.only(left: 10, right: 30.0, bottom: 10.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Theme.of(context).backgroundColor,
-        ),
-        child: Row(
-          children: [
-            SizedBox(
-              height: 80,
-              child: Image.network(
-                "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1629842712000",
-                fit: BoxFit.fill,
+      child: Stack(
+        children: [
+          Container(
+            height: 100,
+            margin: const EdgeInsets.only(left: 10, right: 30.0, bottom: 10.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Theme.of(context).backgroundColor,
+            ),
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 80,
+                  child: Image.network(
+                    "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1629842712000",
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "iPhone XR",
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "\$ 1,099",
+                        style: Theme.of(context).textTheme.subtitle2,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            right: 15,
+            top: 30,
+            child: SizedBox(
+              height: 30,
+              width: 30,
+              child: MaterialButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                padding: const EdgeInsets.all(0.0),
+                color: AppColors.favColor,
+                child: const Icon(Icons.close, color: Colors.white),
+                onPressed: () {},
               ),
             ),
-            const SizedBox(width: 10),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "iPhone XR",
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    "\$ 1,099",
-                    style: Theme.of(context).textTheme.subtitle2,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
