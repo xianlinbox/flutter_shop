f5b0c54faad483609f4096c19166832e864d3042
xianning liu <xianlinbox@gmail.com>
2022-03-06 19:39:41 -0600

    add margin for cart empty screen

--- lib/cart/cart_empty.dart
+++ lib/cart/cart_empty.dart
@@ -5,53 +5,50 @@ class CartEmpty extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height * 0.4,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('lib/assets/images/emptycart.png'),
-                fit: BoxFit.fill,
-              ),
-            ),
+    return Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      Container(
+        margin: const EdgeInsets.only(top: 100),
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height * 0.4,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/assets/images/emptycart.png'),
+            fit: BoxFit.fill,
           ),
-          Text('Your Cart Is Empty',
-              style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).textSelectionTheme.selectionColor)),
-          const SizedBox(height: 20),
-          Text('Looks like you didn\'t add anything to your cart yet.',
+        ),
+      ),
+      Text('Your Cart Is Empty',
+          style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).textSelectionTheme.selectionColor)),
+      const SizedBox(height: 20),
+      Text('Looks like you didn\'t add anything to your cart yet.',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).disabledColor)),
+      const SizedBox(height: 20),
+      SizedBox(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.width * 0.15,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            side: const BorderSide(color: Colors.red),
+            primary: Colors.redAccent,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+          ),
+          child: Text('Shopping Now',
               textAlign: TextAlign.center,
               style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).disabledColor)),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: MediaQuery.of(context).size.width * 0.15,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                side: const BorderSide(color: Colors.red),
-                primary: Colors.redAccent,
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-              ),
-              child: Text('Shopping Now',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.w600,
-                      color:
-                          Theme.of(context).textSelectionTheme.selectionColor)),
-            ),
-          )
-        ]);
+                  fontSize: 26,
+                  fontWeight: FontWeight.w600,
+                  color: Theme.of(context).textSelectionTheme.selectionColor)),
+        ),
+      )
+    ]);
   }
 }
