f91213c01e64648b4d8f1bda275e979fc2591d78
xianning liu <xianlinbox@gmail.com>
2022-03-14 18:59:05 -0500

    add brand products screen

---
+++ lib/products/brand_products.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BrandProducts extends StatefulWidget {
+  const BrandProducts({Key? key}) : super(key: key);
+  static const routeName = '/brands';
+
+  @override
+  State<BrandProducts> createState() => _BrandProductsState();
+}
+
+class _BrandProductsState extends State<BrandProducts> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
