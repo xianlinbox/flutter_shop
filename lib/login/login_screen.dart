844bdef0fa2da1786615586249d4ee09e4a9ca11
xianning liu <xianlinbox@gmail.com>
2022-03-23 12:29:32 -0500

    add email & password form

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/shared/app_icons.dart';
 import 'package:flutter_shop/shared/colors.dart';
 import 'package:wave/config.dart';
 import 'package:wave/wave.dart';
@@ -62,6 +63,110 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(
                 height: 30,
               ),
+              Form(
+                  child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: TextFormField(
+                      key: const ValueKey('email'),
+                      validator: (value) {
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                      onEditingComplete: () {},
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          border: const UnderlineInputBorder(),
+                          filled: true,
+                          prefixIcon: const Icon(Icons.email),
+                          labelText: 'Email Address',
+                          fillColor: Theme.of(context).backgroundColor),
+                      onSaved: (value) {},
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: TextFormField(
+                      key: const ValueKey('password'),
+                      validator: (value) {
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                      onEditingComplete: () {},
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          border: const UnderlineInputBorder(),
+                          filled: true,
+                          prefixIcon: const Icon(Icons.lock),
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                // _obscureText = !_obscureText;
+                              });
+                            },
+                            child: const Icon(Icons.visibility),
+                          ),
+                          labelText: 'Password',
+                          fillColor: Theme.of(context).backgroundColor),
+                      onSaved: (value) {},
+                      obscureText: true,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 2, horizontal: 20),
+                      child: TextButton(
+                          onPressed: () {
+                            // Navigator.pushNamed(context, '');
+                          },
+                          child: Text(
+                            'Forget password?',
+                            style: TextStyle(
+                                color: Colors.blue.shade900,
+                                decoration: TextDecoration.underline),
+                          )),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const SizedBox(width: 10),
+                      ElevatedButton(
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              side:
+                                  BorderSide(color: AppColors.backgroundColor),
+                            ),
+                          )),
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                'Login',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 17),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Icon(
+                                AppIcons.user,
+                                size: 18,
+                              )
+                            ],
+                          )),
+                      const SizedBox(width: 20),
+                    ],
+                  ),
+                ],
+              ))
             ],
           ),
         )
