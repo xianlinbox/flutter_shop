b1f12b71248200ecedc4f7fb36f4f56253afa581
xianning liu <xianlinbox@gmail.com>
2022-03-22 19:18:04 -0500

    add annimation for background page

--- lib/login/landing_screen.dart
+++ lib/login/landing_screen.dart
@@ -9,7 +9,36 @@ class LandingScreen extends StatefulWidget {
   State<LandingScreen> createState() => _LandingScreenState();
 }
 
-class _LandingScreenState extends State<LandingScreen> {
+class _LandingScreenState extends State<LandingScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 20));
+    _animation =
+        CurvedAnimation(parent: _animationController, curve: Curves.linear)
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((animationStatus) {
+            if (animationStatus == AnimationStatus.completed) {
+              _animationController.reset();
+              _animationController.forward();
+            }
+          });
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +53,7 @@ class _LandingScreenState extends State<LandingScreen> {
           height: double.infinity,
           width: double.infinity,
           fit: BoxFit.cover,
+          alignment: FractionalOffset(_animation.value, 0),
         ),
         // CachedNetworkImage(
         //   imageUrl:
