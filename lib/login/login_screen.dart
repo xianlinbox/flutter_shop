d5dc49b7df535382c47afc18b0f6f8c76aa5a6f6
xianning liu <xianlinbox@gmail.com>
2022-03-24 16:42:05 -0500

    validate email & password when click login

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/shared/app_icons.dart';
 import 'package:flutter_shop/shared/colors.dart';
-import 'package:flutter_shop/shared/email_validator.dart';
+import 'package:flutter_shop/shared/input_validator.dart';
 import 'package:wave/config.dart';
 import 'package:wave/wave.dart';
 
@@ -15,6 +15,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool _obscureText = true;
+  final _formKey = GlobalKey<FormState>();
   String _emailAddress = '';
   String _password = '';
 
@@ -69,113 +70,119 @@ class _LoginScreenState extends State<LoginScreen> {
                 height: 30,
               ),
               Form(
+                  key: _formKey,
                   child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: TextFormField(
-                      key: const ValueKey('email'),
-                      validator: (value) {
-                        return EmailValidator.isValidEmail(value)
-                            ? null
-                            : 'Please enter a valid email address';
-                      },
-                      textInputAction: TextInputAction.next,
-                      onEditingComplete: () {},
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                          border: const UnderlineInputBorder(),
-                          filled: true,
-                          prefixIcon: const Icon(Icons.email),
-                          labelText: 'Email Address',
-                          fillColor: Theme.of(context).backgroundColor),
-                      onSaved: (value) {},
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: TextFormField(
-                      key: const ValueKey('password'),
-                      validator: (value) {
-                        return null;
-                      },
-                      textInputAction: TextInputAction.next,
-                      onEditingComplete: () {},
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                          border: const UnderlineInputBorder(),
-                          filled: true,
-                          prefixIcon: const Icon(Icons.lock),
-                          suffixIcon: GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _obscureText = !_obscureText;
-                              });
-                            },
-                            child: Icon(_obscureText
-                                ? Icons.visibility
-                                : Icons.visibility_off),
-                          ),
-                          labelText: 'Password',
-                          fillColor: Theme.of(context).backgroundColor),
-                      onSaved: (value) {},
-                      obscureText: _obscureText,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 2, horizontal: 20),
-                      child: TextButton(
-                          onPressed: () {
-                            // Navigator.pushNamed(context, '');
-                          },
-                          child: Text(
-                            'Forget password?',
-                            style: TextStyle(
-                                color: Colors.blue.shade900,
-                                decoration: TextDecoration.underline),
-                          )),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      const SizedBox(width: 10),
-                      ElevatedButton(
-                          style: ButtonStyle(
-                              shape: MaterialStateProperty.all<
-                                  RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                              side:
-                                  BorderSide(color: AppColors.backgroundColor),
-                            ),
-                          )),
-                          onPressed: () {},
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Text(
-                                'Login',
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: TextFormField(
+                          key: const ValueKey('email'),
+                          validator: (value) {
+                            return InputValidator.isValidEmail(value)
+                                ? null
+                                : 'Please enter a valid email address';
+                          },
+                          textInputAction: TextInputAction.next,
+                          onEditingComplete: () {},
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                              border: const UnderlineInputBorder(),
+                              filled: true,
+                              prefixIcon: const Icon(Icons.email),
+                              labelText: 'Email Address',
+                              fillColor: Theme.of(context).backgroundColor),
+                          onSaved: (value) {},
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: TextFormField(
+                          key: const ValueKey('password'),
+                          validator: (value) {
+                            return InputValidator.isValidPassword(value ?? '')
+                                ? null
+                                : 'Please enter a valid password';
+                          },
+                          textInputAction: TextInputAction.next,
+                          onEditingComplete: () {},
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                              border: const UnderlineInputBorder(),
+                              filled: true,
+                              prefixIcon: const Icon(Icons.lock),
+                              suffixIcon: GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    _obscureText = !_obscureText;
+                                  });
+                                },
+                                child: Icon(_obscureText
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                              ),
+                              labelText: 'Password',
+                              fillColor: Theme.of(context).backgroundColor),
+                          onSaved: (value) {},
+                          obscureText: _obscureText,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 2, horizontal: 20),
+                          child: TextButton(
+                              onPressed: () {
+                                // Navigator.pushNamed(context, '');
+                              },
+                              child: Text(
+                                'Forget password?',
                                 style: TextStyle(
-                                    fontWeight: FontWeight.w500, fontSize: 17),
-                              ),
-                              const SizedBox(
-                                width: 5,
-                              ),
-                              Icon(
-                                AppIcons.user,
-                                size: 18,
-                              )
-                            ],
-                          )),
-                      const SizedBox(width: 20),
+                                    color: Colors.blue.shade900,
+                                    decoration: TextDecoration.underline),
+                              )),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                              style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30.0),
+                                  side: BorderSide(
+                                      color: AppColors.backgroundColor),
+                                ),
+                              )),
+                              onPressed: () {
+                                _formKey.currentState?.validate();
+                              },
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    'Login',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 17),
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Icon(
+                                    AppIcons.user,
+                                    size: 18,
+                                  )
+                                ],
+                              )),
+                          const SizedBox(width: 20),
+                        ],
+                      ),
                     ],
-                  ),
-                ],
-              ))
+                  ))
             ],
           ),
         )
--- lib/shared/input_validator.dart
+++ lib/shared/input_validator.dart
@@ -1,7 +1,11 @@
-class EmailValidator {
+class InputValidator {
   static bool isValidEmail(String? value) {
     return RegExp(
             r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
         .hasMatch(value ?? '');
+  }
+
+  static bool isValidPassword(String value) {
+    return value.isNotEmpty && value.length >= 8;
   }
 }
